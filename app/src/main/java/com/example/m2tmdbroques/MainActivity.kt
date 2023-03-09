0801f13d0c7332ab721a946d9fe3716d6ee56398
AlexisRqs <alexisrqs@outlook.fr>
2023-03-09 12:58:19 +0100

    Added Http client

--- app/src/main/java/com/example/m2tmdbroques/MainActivity.kt
+++ app/src/main/java/com/example/m2tmdbroques/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.m2tmdbroques
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.m2tmdbroques.model.PersonPopularResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tmdbapi = ApiClient.instance.create(ITmdbApi::class.java)
+
+        val call = tmdbapi.getPopularPerson(TMDB_API_KEY, 1)
+
+        call.enqueue(object : Callback<PersonPopularResponse> {
+            override fun onResponse(
+                call: Call<PersonPopularResponse>,
+                response: Response<PersonPopularResponse>
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(call: Call<PersonPopularResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        }   )
     }
 }
